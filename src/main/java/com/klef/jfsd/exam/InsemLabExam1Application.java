71c51f9d3c88c0c189c8d6a5e390dfd00d081eea
Sravan <2200030818@kluniversity.in>
2024-12-07 08:43:51 +0530

    First  Commit

---
+++ src/main/java/com/klef/jfsd/exam/InsemLabExam1Application.java
@@ -0,0 +1,14 @@
+package com.klef.jfsd.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsemLabExam1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsemLabExam1Application.class, args);
+		System.out.println("Project is Working!!!!");
+	}
+
+}
